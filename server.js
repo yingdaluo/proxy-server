8a1eb59ab07320f403aecc6ed3ba0ebc59d913cb
Yingda Luo <yluo@medallia.com>
2018-01-15 23:00:18 -0800

    DDDD

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var proxy = require('express-http-proxy');
 var app = require('express')();
 
-app.use('/', proxy('3cjn1rwq.api.lncld.net', {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var appId = process.env.APP_ID;
+var appKey = process.env.APP_KEY;
+var serviceDomain = process.env.SERVICE_DOMAIN;
+app.use('/', proxy(serviceDomain, {
   https: true,
   proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
     // you can update headers
     proxyReqOpts.headers['Content-Type'] = 'application/json';
-    proxyReqOpts.headers['X-LC-Id'] = '3CJn1RwqYUdV53DwGEJpHA1C-gzGzoHsz';
-    proxyReqOpts.headers['X-LC-Key'] = 'z2bruTumSx3Y1KBTDkarEkhh';
+    proxyReqOpts.headers['X-LC-Id'] = appId;
+    proxyReqOpts.headers['X-LC-Key'] = appKey;
     return proxyReqOpts;
   }
 }));
 
-app.listen(5050);
+app.listen(server_port);
